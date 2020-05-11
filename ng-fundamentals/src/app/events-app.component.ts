f2e89c28c33226e1ff8489cccc86df1e4f46375a
Mark Struck <markstruck@comcast.net>
2020-05-10 22:03:21 -0500

    added external libraries

--- ng-fundamentals/src/app/events-app.component.ts
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Hello World!</h1>'
+  template: `<h1>Hello World!</h1>
+              <img src="assets/images/basic-shield.png">
+            `
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
